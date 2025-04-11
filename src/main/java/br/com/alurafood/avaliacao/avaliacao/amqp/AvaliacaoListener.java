caeaa87881080e53321481f94ec66f375ce1943c
Leonardo Isaias <leoisaias135@gmail.com>
2025-04-11 02:43:14 -0300

    Adicionado configurações para lidar com mensagens que não foram enviadas, então foi criado uma nova exchange DLX e uma fila DLQ

--- src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
+++ src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
@@ -9,19 +9,22 @@ import org.springframework.stereotype.Component;
 public class AvaliacaoListener {
     @RabbitListener(queues = "pagamentos.detalhes-avaliacao")
     public void recebeMensagem(@Payload PagamentoDto pagamento) {
+
+        if (pagamento.getNumero().equals("0000")) {
+            throw new RuntimeException("não consegui processar");
+        }
+
         String mensagem = """
-                Necessário criar registro de avaliação para o pedido: %s 
+                Necessário criar registro de avaliação para o pedido: %s
                 Id do pagamento: %s
                 Nome do cliente: %s
                 Valor R$: %s
-                Status: %s 
-                Pagador: %s
+                Status: %s
                 """.formatted(pagamento.getPedidoId(),
                 pagamento.getId(),
                 pagamento.getNome(),
                 pagamento.getValor(),
-                pagamento.getStatus(),
-                pagamento.getNome());
+                pagamento.getStatus());
 
         System.out.println(mensagem);
     }
